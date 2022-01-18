1433c307da7ea4085cf168f56a0200869666d743
AleksandrVoroninSME <aleksandr_av@bk.ru>
2022-01-18 22:17:37 +0300

    Task2.4.2 (UPD1)

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -8,7 +8,8 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{
-                HibernateConfig.class
+                HibernateConfig.class,
+                SecurityConfig.class
         };
     }
 
